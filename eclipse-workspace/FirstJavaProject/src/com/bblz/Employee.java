5055586519a209b8574857c96ef7cbaf61f5d659
p031998 <panagarwal03@gmail.com>
2021-02-10 21:29:09 +0530

    Employee Presence

---
+++ eclipse-workspace/FirstJavaProject/src/com/bblz/Employee.java
@@ -0,0 +1,13 @@
+package com.bblz;
+
+public class Employee {
+	public static void main(String[] args) {
+		int IS_FULL_TIME=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME)
+			System.out.println("is present");
+		else
+			System.out.println("is absent");
+	}
+
+}
