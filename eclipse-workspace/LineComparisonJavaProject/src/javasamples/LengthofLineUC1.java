4e36365af1099b1dfde76a35cae412a3af8101b6
p031998 <panagarwal03@gmail.com>
2021-02-11 22:40:34 +0530

    Length of Line

---
+++ eclipse-workspace/LineComparisonJavaProject/src/javasamples/LengthofLineUC1.java
@@ -0,0 +1,10 @@
+package javasamples;
+
+public class LengthofLineUC1 {
+	public static void main(String[] args) {
+		int x1=1,x2=4,y1=1,y2=4;
+		double length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println("Length of the Line is:"+length);
+	}
+
+}
