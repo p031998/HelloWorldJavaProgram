f1e6cf179262733fcb03d8ad2861dd97ee6d1df9
p031998 <68192452+p031998@users.noreply.github.com>
2021-02-12 00:59:27 +0530

    Add files via upload

---
+++ eclipse-workspace/FirstJavaProject/src/com/bblz/EmployeeWageUC6.java
@@ -0,0 +1,37 @@
+package com.bblz;
+
+public class EmployeeWageUC6 {
+	 public static final int IS_FULL_TIME=2;
+	 public static final int EMP_RATE_PER_HOUR=20;
+	 public static final int IS_PART_TIME=1;
+	 public static final int NUM_OF_WORKING_DAYS=2;
+	 public static final int MAX_HRS_IN_MONTH=10;
+	 public static void main(String[] args) {
+		int totalEmpWage=0;
+		int totalWorkingDays=0;
+		int totalEmpHrs=0;
+		double empCheck=Math.floor(Math.random()*10)%3;
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+		int empHrs=0;
+		totalWorkingDays++;
+		switch((int)empCheck) {
+		case IS_FULL_TIME:
+			empHrs=8;
+			break;
+		case IS_PART_TIME:
+			empHrs=4;
+			break;
+			default:
+			empHrs=0;
+		}
+		totalEmpHrs+=empHrs;
+		int empWage=empHrs*EMP_RATE_PER_HOUR;
+		totalEmpWage+=empWage;
+		System.out.println("Emp Wage:"+empWage);
+	 }
+		System.out.println("Total Emp Wage:"+totalEmpWage);
+	 }
+}
+
+
+
